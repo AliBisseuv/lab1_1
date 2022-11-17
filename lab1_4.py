15690bd5834876ffc377adf8c4ef3c609060dc53
alibisseuv <118120584+AliBisseuv@users.noreply.github.com>
2022-11-17 16:49:46 +0600

    Update lab1_4.py

--- lab1_4.py
+++ lab1_4.py
@@ -1,31 +1,37 @@
+# Задание № 4 
+
 # Инициализация
-print('НАХОЖДЕНИЕ ВРЕМЕНИ ЗАВЕРШЕНИЯ УРОКА')
+print('Нахождение времени завершения урока')
+
 # Бесконечный цикл
 while True:
+
+    # Пригласительная строка
     print('\nДля завершения программы введите любой не числовой символ или отрицательное число (включая 0)\n')
 
     lesson = input('Введите номер урока: ')
 
-    if not lesson.isdigit(): break
-    lesson = int(lesson)
-    # Введено отрицательное число
-    if lesson<=0 : break
+    # Выход из бесконечного цикл
+    if not lesson.isdigit():
+        break
+    elif int(lesson) == 0: break
 
-# Ограничения по количеству уроков в день
-    # Если урок 11 ...
-    if lesson > 10:
-         print('Занятия в школе закончились! Все ученики ушли домой!')
-         continue
+    # Находжение времени завершения урока
+    
+    # Ограничение количества уроков (не более 10)
+    if int(lesson) > 10:
+        print("В школе занятия идут до 18:00!\n")
+        continue
 
     # Урок четный ...
-    if (lesson % 2) == 0:
-        all_time_in_minutes = ((lesson // 2) * 110) - 15
+    if (int(lesson) % 2) == 0:
+        all_time_in_minutes = ((int(lesson) // 2) *110) - 15
         
     # Урок нечетный ...
-    if (lesson % 2) > 0:
-        all_time_in_minutes = ((lesson // 2) * 110) + 45
+    if (int(lesson) % 2) > 0:
+        all_time_in_minutes = ((int(lesson) // 2) *110) + 45
         
     print(f'Время: {all_time_in_minutes // 60 + 9:02d}:{all_time_in_minutes % 60:02d}')
 
-# Завершение работы
+# Завершение работы программы
 print('\n\aПока, пока ...')
